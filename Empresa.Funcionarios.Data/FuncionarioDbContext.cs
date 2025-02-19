8754b7bd9bc2b8b58f314f451994315080ab4ce4
Julio Bacoli <julio-microlins@hotmail.com>
2025-02-19 00:23:23 -0300

    migrations gerada com sql server

--- Empresa.Funcionarios.Data/FuncionarioDbContext.cs
+++ Empresa.Funcionarios.Data/FuncionarioDbContext.cs
@@ -5,8 +5,18 @@ namespace Empresa.Funcionarios.Data;
 
 public class FuncionarioDbContext : DbContext
 {
+    public FuncionarioDbContext() { }
+
     public FuncionarioDbContext(DbContextOptions<FuncionarioDbContext> options) : base(options) { }
     public DbSet<Funcionario> Funcionarios { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost;Database=EmpresaFuncionarios;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
---
+++ Empresa.Funcionarios.Data/Migrations/20250219032151_InitialCreate.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Empresa.Funcionarios.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Funcionarios",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Sobrenome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NumeroDocumento = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Telefones = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    GestorId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    DataNascimento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    SenhaHash = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Funcionarios", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Funcionarios_NumeroDocumento",
+                table: "Funcionarios",
+                column: "NumeroDocumento",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Funcionarios");
+        }
+    }
+}
